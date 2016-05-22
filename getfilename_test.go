cf5a74b5b7beea135a1a687294be2d07b2e62e08
Frank Xu <frank.xu@northbright.com>
2016-05-22 14:40:10 +0800

    Remove 'resp' in error message

--- getfilename_test.go
+++ getfilename_test.go
@@ -11,7 +11,7 @@ func ExampleGetFileName() {
 	url := "https://github.com/northbright/plants/archive/master.zip"
 
 	if f, err := httputil.GetFileName(url); err != nil {
-		fmt.Fprintf(os.Stderr, "GetFileName(%v, resp) err: %v\n", url, err)
+		fmt.Fprintf(os.Stderr, "GetFileName(%v) err: %v\n", url, err)
 	} else {
 		fmt.Fprintf(os.Stderr, "GetFileName() succeeded: file name = %v\n", f)
 	}
