233151e08dbb9afb75186ce9b83f8e8ae222ece2
Frank Xu <frank.xu@northbright.com>
2022-12-01 14:32:05 +0800

    Add ErrNot200or206

--- range.go
+++ range.go
@@ -1,6 +1,7 @@
 package httputil
 
 import (
+	"errors"
 	"net/http"
 )
 
@@ -8,13 +9,23 @@ const (
 	keyAcceptRanges = "Accept-Ranges"
 )
 
+var (
+	// ErrNot200or206 represents the error that the status code is not 200 or 206.
+	ErrNot200or206 = errors.New("status code is not 200 or 206")
+)
+
 // IsRangeSupported returns if range is supported by the server on the URL.
 func IsRangeSupported(url string) (bool, error) {
+	// Do HTTP request with HEAD method(without body)
 	resp, err := http.Head(url)
 	if err != nil {
 		return false, err
 	}
-	defer resp.Body.Close()
+
+	// Check if status code is 200 or 206.
+	if resp.StatusCode != 200 && resp.StatusCode != 206 {
+		return false, ErrNot200or206
+	}
 
 	if resp.Header.Get(keyAcceptRanges) != "bytes" {
 		return false, nil
